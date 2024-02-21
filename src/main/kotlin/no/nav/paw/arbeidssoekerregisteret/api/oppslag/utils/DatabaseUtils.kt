69190f9502811e6efff0b5754ec662aef9047a78
Aurora Christine Hofman <aurora.christine.hofman@nav.no>
2024-02-21 13:20:38 +0100

    clean dev database

--- src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/api/oppslag/utils/DatabaseUtils.kt
+++ src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/api/oppslag/utils/DatabaseUtils.kt
@@ -14,6 +14,7 @@ fun main() {
 }
 
 fun migrateDatabase(dataSource: DataSource) {
+    cleanDatabase(dataSource) //TODO: slett
     Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate()
 }
 
