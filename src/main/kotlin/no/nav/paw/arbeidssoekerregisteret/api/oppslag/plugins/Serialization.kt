fcd2fed9ea7ddb3f80ede17f0b22c764bbd8d7cc
Nils Martin Sande <nils.martin.sande@nav.no>
2024-04-04 14:49:20 +0200

    Fikset 'instant' + formatering i openapi doc

--- src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/api/oppslag/plugins/Serialization.kt
+++ src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/api/oppslag/plugins/Serialization.kt
@@ -3,6 +3,7 @@ package no.nav.paw.arbeidssoekerregisteret.api.oppslag.plugins
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.serialization.jackson.jackson
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
@@ -11,11 +12,10 @@ import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
         jackson {
-            jackson {
-                disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-                disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-                registerModule(JavaTimeModule())
-            }
+            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            registerModule(JavaTimeModule())
+            registerKotlinModule()
         }
     }
 }
