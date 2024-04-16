213dedf64cc880931f7ab206d767f4d290bd82f1
Nils Martin Sande <nils.martin.sande@nav.no>
2024-04-16 07:38:14 +0200

    La til ktor percentiles histogram

--- src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/api/oppslag/plugins/Metrics.kt
+++ src/main/kotlin/no/nav/paw/arbeidssoekerregisteret/api/oppslag/plugins/Metrics.kt
@@ -7,8 +7,10 @@ import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics
 import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics
 import io.micrometer.core.instrument.binder.kafka.KafkaClientMetrics
 import io.micrometer.core.instrument.binder.system.ProcessorMetrics
+import io.micrometer.core.instrument.distribution.DistributionStatisticConfig
 import io.micrometer.prometheus.PrometheusMeterRegistry
 import org.apache.kafka.clients.consumer.KafkaConsumer
+import java.time.Duration
 
 fun Application.configureMetrics(
     prometheusMeterRegistry: PrometheusMeterRegistry,
@@ -22,5 +24,15 @@ fun Application.configureMetrics(
                 JvmGcMetrics(),
                 ProcessorMetrics()
             ) + consumer.map { KafkaClientMetrics(it) }
+        distributionStatisticConfig =
+            DistributionStatisticConfig.builder()
+                .percentilesHistogram(true)
+                .maximumExpectedValue(Duration.ofSeconds(1).toNanos().toDouble())
+                .minimumExpectedValue(Duration.ofMillis(10).toNanos().toDouble())
+                .serviceLevelObjectives(
+                    Duration.ofMillis(50).toNanos().toDouble(),
+                    Duration.ofMillis(100).toNanos().toDouble()
+                )
+                .build()
     }
 }
