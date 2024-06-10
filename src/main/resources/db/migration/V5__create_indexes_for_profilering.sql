4d61fb64937c99fb6eea41bd887e1e311679b788
robertkittilsen <robert.graarup.kittilsen@nav.no>
2024-06-10 16:09:08 +0200

    Legger til manglende index for profilering

---
+++ src/main/resources/db/migration/V5__create_indexes_for_profilering.sql
@@ -0,0 +1,4 @@
+CREATE INDEX idx_profilering_profilering_id ON profilering(profilering_id);
+CREATE INDEX idx_profilering_periode_id ON profilering(periode_id);
+CREATE INDEX idx_profilering_opplysninger_om_arbeidssoeker_id ON profilering(opplysninger_om_arbeidssoeker_id);
+CREATE INDEX idx_profilering_sendt_inn_av_id ON profilering(sendt_inn_av_id);
